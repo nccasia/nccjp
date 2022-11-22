3b20c154bc15fda937a655bd73026e66fff15fe0
Nguyen Quan <quan.nguyendinh@ncc.asia>
2022-11-22 21:28:56 +0700

    update UI

---
+++ assets/js/viewmap.js
@@ -0,0 +1,29 @@
+const btnCloseMap = document.querySelector(".btn-close-modal");
+const modalContactMap = document.querySelector(".modal-contact-map");
+const contactLinks = document.querySelectorAll(".contact-link");
+const mapContents = document.querySelectorAll(".map-content");
+
+
+console.log("map content : ", mapContents);
+btnCloseMap.addEventListener("click", ()=>{
+    modalContactMap.classList.remove("modal-contact-map-active");
+})
+
+// for(let i = 0; i < contactLinks.length; ++i){
+
+//     contactLinks[i].addEventListener("click", ()=>{
+//         modalContactMap.classList.add("modal-contact-map-active")
+//     })
+    
+// }
+contactLinks[0].addEventListener("click", ()=>{
+    console.log("hLOO")
+    modalContactMap.classList.add("modal-contact-map-active")
+    mapContents[0].classList.add("map-content-active")
+    mapContents[1].classList.remove("map-content-active")
+})
+contactLinks[1].addEventListener("click", ()=>{
+    modalContactMap.classList.add("modal-contact-map-active")
+    mapContents[0].classList.remove("map-content-active")
+    mapContents[1].classList.add("map-content-active")
+})
